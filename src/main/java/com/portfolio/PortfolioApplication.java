3fff5218a27b029fe9303d87ba66cd4fb5daf0e7
teegi10 <mateoteggi16@gmail.com>
2022-07-08 11:00:55 -0300

    agrego cors

--- src/main/java/com/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/PortfolioApplication.java
@@ -1,7 +1,13 @@
 package com.portfolio;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 
@@ -10,6 +16,21 @@ public class PortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
+	}
+	@Bean
+	public CorsFilter corsFilter() {
+	CorsConfiguration corsConfiguration = new CorsConfiguration();
+	corsConfiguration.setAllowCredentials(true);
+	corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolio-53d21.web.app"));
+	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+	"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+	"Access-Control-Requested-Method", "Access-Control-Allow-Credentials"));
+	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Control-Type", "Accept", "Authorization",
+	"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","OPTIONS"));
+	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+	urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+	return new CorsFilter ( urlBasedCorsConfigurationSource);
 	}
 
 }
