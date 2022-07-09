eb70350fc1239a16768b2d695aee62353f9671d4
teegi10 <mateoteggi16@gmail.com>
2022-07-09 18:10:42 -0300

    agrego servidor local al cors

--- src/main/java/com/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/PortfolioApplication.java
@@ -21,7 +21,7 @@ public class PortfolioApplication {
 	public CorsFilter corsFilter() {
 	CorsConfiguration corsConfiguration = new CorsConfiguration();
 	corsConfiguration.setAllowCredentials(true);
-	corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolio-53d21.web.app"));
+	corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolio-53d21.web.app","http://localhost:4200"));
 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 	"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 	"Access-Control-Requested-Method", "Access-Control-Allow-Credentials"));
